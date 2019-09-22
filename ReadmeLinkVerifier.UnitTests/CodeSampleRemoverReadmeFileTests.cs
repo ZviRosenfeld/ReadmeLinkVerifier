9ac2c39b1a6c5df2dd1439cd11632625bcf686b3
Zvi Rosenfeld <Zvi.Rosenfeld@checkmarx.com>
2019-09-22 08:28:42 +0300

    Added (failing) CodeSample tests

---
+++ ReadmeLinkVerifier.UnitTests/CodeSampleRemoverReadmeFileTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ReadmeLinkVerifier.ReadmeFiles;
+using ReadmeLinkVerifier.UnitTests.Utils;
+
+namespace ReadmeLinkVerifier.UnitTests
+{
+    [TestClass]
+    public class CodeSampleRemoverReadmeFileTests
+    {
+        private const string CodeSampleText = "CodeSample";
+
+        [TestMethod]
+        [DataRow("`"+ CodeSampleText + "`")]
+        [DataRow("``" + CodeSampleText + "``")]
+        [DataRow("```" + CodeSampleText + "```")]
+        [DataRow("``` " + CodeSampleText + " ```")]
+        [DataRow("```\n" + CodeSampleText + "\n```")]
+        [DataRow("```\n" + CodeSampleText + "```")]
+        [DataRow("````" + CodeSampleText + "````")]
+        [DataRow("```" + CodeSampleText + "````")]
+        [DataRow("```" + CodeSampleText + "`````")]
+        [DataRow("```" + CodeSampleText)]
+        [DataRow("``` SampleStart ``" + CodeSampleText)]
+        [DataRow("```` SampleStart ```" + CodeSampleText)]
+        [DataRow("` NotSample ``" + CodeSampleText + "``")]
+        public void IgnoreCodeSamples(string codeSample)
+        {
+            var readmeFile = new CodeSampleRemoverReadmeFile(new StringReadmeFile("", codeSample));
+            var text = readmeFile.GetAllText();
+            Assert.IsFalse(text.Contains(CodeSampleText), "Text shouldn't have contained the code samples. Text = " + text);
+        }
+
+        [TestMethod]
+        [DataRow("`" + CodeSampleText + "``")]
+        [DataRow("``" + CodeSampleText + "```")]
+        [DataRow("`" + CodeSampleText)]
+        [DataRow("``" + CodeSampleText)]
+        [DataRow("\\`" + CodeSampleText + "\\`")]
+        [DataRow("``` Sample ````" + CodeSampleText + "````")]
+        public void NotCodeSamples_DontIgnore(string codeSample)
+        {
+            var readmeFile = new CodeSampleRemoverReadmeFile(new StringReadmeFile("", codeSample));
+            var text = readmeFile.GetAllText();
+            Assert.IsTrue(text.Contains(CodeSampleText), "Text should have contained the code samples. Text = " + text);
+        }
+    }
+}
