5d84bbfacd82409bda1853767f58967a69cc05b7
Zvi Rosenfeld <Zvi.Rosenfeld@checkmarx.com>
2019-09-22 12:27:27 +0300

    Ignore code samples (and a lot more)

--- ReadmeLinkVerifier.UnitTests/CodeSampleRemoverReadmeFileTests.cs
+++ ReadmeLinkVerifier.UnitTests/CodeSampleRemoverReadmeFileTests.cs
@@ -24,6 +24,7 @@ namespace ReadmeLinkVerifier.UnitTests
         [DataRow("``` SampleStart ``" + CodeSampleText)]
         [DataRow("```` SampleStart ```" + CodeSampleText)]
         [DataRow("` NotSample ``" + CodeSampleText + "``")]
+        [DataRow("` ``` `" + CodeSampleText)]
         public void IgnoreCodeSamples(string codeSample)
         {
             var readmeFile = new CodeSampleRemoverReadmeFile(new StringReadmeFile("", codeSample));
@@ -38,11 +39,30 @@ namespace ReadmeLinkVerifier.UnitTests
         [DataRow("``" + CodeSampleText)]
         [DataRow("\\`" + CodeSampleText + "\\`")]
         [DataRow("``` Sample ````" + CodeSampleText + "````")]
+        [DataRow("` `` `" + CodeSampleText)]
         public void NotCodeSamples_DontIgnore(string codeSample)
         {
             var readmeFile = new CodeSampleRemoverReadmeFile(new StringReadmeFile("", codeSample));
             var text = readmeFile.GetAllText();
-            Assert.IsTrue(text.Contains(CodeSampleText), "Text should have contained the code samples. Text = " + text);
+            Assert.IsTrue(text.Contains(CodeSampleText), "Text should have contained the false code samples. Text = " + text);
+        }
+
+        [TestMethod]
+        public void TwoCodeSamples_IgnoreBoth()
+        {
+            var NotCodeSampleText = "NotCodeSample";
+            var readmeFile = new CodeSampleRemoverReadmeFile(new StringReadmeFile("", $"``` {CodeSampleText} ``` {NotCodeSampleText} ``` {CodeSampleText} ```"));
+            var text = readmeFile.GetAllText();
+            Assert.IsFalse(text.Contains(CodeSampleText), "Text shouldn't have contained the code samples. Text = " + text);
+            Assert.IsTrue(text.Contains(NotCodeSampleText), "Text should have contained the false code samples. Text = " + text);
+        }
+
+        [TestMethod]
+        public void CodeSampleContainsNewLines_NewLinesNotLost()
+        {
+            var readmeFile = new CodeSampleRemoverReadmeFile(new StringReadmeFile("", $" Before``` {Environment.NewLine} Sample {Environment.NewLine} ``` After"));
+            var text = readmeFile.GetAllText();
+            Assert.AreEqual(4, text.Split(new[] { Environment.NewLine }, StringSplitOptions.None), "NewLines lost");
         }
     }
 }
--- ReadmeLinkVerifier/ReadmeFiles/CodeSampleRemoverReadmeFile.cs
+++ ReadmeLinkVerifier/ReadmeFiles/CodeSampleRemoverReadmeFile.cs
@@ -12,17 +12,91 @@ namespace ReadmeLinkVerifier.ReadmeFiles
             this.inneReadmeFile = inneReadmeFile;
         }
 
+        public string GetRelativePath() => inneReadmeFile.GetRelativePath();
+
         private string text;
         public string GetAllText()
         {
             if (text == null)
             {
                 text = inneReadmeFile.GetAllText();
+                text = RemoveCodeSamples(text);
             }
 
             return text;
         }
-        
-        public string GetRelativePath() => inneReadmeFile.GetRelativePath();
+
+        private string RemoveCodeSamples(string text)
+        {
+            var stringBuilder = new StringBuilder();
+
+            var collectedForOpen = 0; // The ` we've collected while opening the codeSample
+            var collectedForClose = 0; // The ` we've collected while closing the codeSample 
+            var mode = Mode.Normal;
+            for (int i = 0; i < text.Length; i++)
+            {
+                var c = text[i];
+                if (c == '`')
+                {
+                    switch (mode)
+                    {
+                        case Mode.Normal:
+                        case Mode.Opening:
+                            mode = Mode.Opening;
+                            collectedForOpen++;
+                            break;
+                        case Mode.IgnoreText:
+                        case Mode.TentativeIgnoreText:
+                        case Mode.Closing:
+                            mode = Mode.Closing;
+                            collectedForClose++;
+                            break;
+                    }
+                }
+                else
+                {
+                    switch (mode)
+                    {
+                        case Mode.Normal:
+                            stringBuilder.Append(c);
+                            break;
+                        case Mode.Opening:
+                            mode = collectedForOpen >= 3 ? Mode.IgnoreText : Mode.TentativeIgnoreText;
+                            break;
+                        case Mode.Closing:
+                            if (collectedForOpen == collectedForClose)
+                                mode = Mode.Normal;
+                            if (collectedForOpen < collectedForClose)
+                            {
+                                if (collectedForOpen < 3 && collectedForClose < 3)
+                                    mode = Mode.TentativeIgnoreText;
+                                else if (collectedForOpen >= 3 && collectedForClose >= 3)
+                                    mode = Mode.Normal;
+                                else
+                                    mode = Mode.IgnoreText;
+                            }
+                            else // if (collectedForOpen > collectedForClose)
+                            {
+                                if (collectedForOpen < 3)
+                                    mode = Mode.TentativeIgnoreText;
+                                else
+                                    mode = Mode.IgnoreText;
+                            }
+                            break;
+                    }
+                }
+            }
+
+            return stringBuilder.ToString();
+        }
+    }
+
+    enum Mode
+    {
+        Normal,
+        IgnoreText,
+        TentativeIgnoreText, // When we're not yet sure if text needs to be ignored (e.g. when a ` was opened, but we're not sure if it will close
+        Opening,
+        Closing
     }
 }
